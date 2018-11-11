8dc5328b5a85cc6fd141d4903042129f266cf9d8
Cesar Toribio <ctoribio@coordinatedhealth.com>
2018-11-11 01:44:43 -0500

    ftr-cliente-rest - Adding CrossOrigin permission to the Angular App

--- src/main/java/com/cesarjuniort/springboot/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/cesarjuniort/springboot/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.cesarjuniort.springboot.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.cesarjuniort.springboot.apirest.models.services.IClienteService;
 // import com.cesarjuniort.springboot.apirest.models.entity.services.ClienteServiceImpl;
 
 @RestController
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RequestMapping("/api")
 public class ClienteRestController {
 	
