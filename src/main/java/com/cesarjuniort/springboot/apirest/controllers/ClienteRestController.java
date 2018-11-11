18bf9081c3242b65dfa60095cb7a81ea8e05fc09
Cesar Toribio <ctoribio@coordinatedhealth.com>
2018-11-11 01:39:05 -0500

    ftr-cliente-rest - Adding the Clientes controller and the initial import of sample data into sql

---
+++ src/main/java/com/cesarjuniort/springboot/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,26 @@
+package com.cesarjuniort.springboot.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cesarjuniort.springboot.apirest.models.entity.Cliente;
+import com.cesarjuniort.springboot.apirest.models.services.IClienteService;
+// import com.cesarjuniort.springboot.apirest.models.entity.services.ClienteServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+	
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> index(){
+		return clienteService.findAll();
+	}
+
+}
