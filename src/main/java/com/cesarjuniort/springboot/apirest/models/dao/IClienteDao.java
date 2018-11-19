90f2897dc7de16eb1d51dd1c8addcfe6195b19de
Cesar Toribio <ctoribio@coordinatedhealth.com>
2018-11-19 10:35:57 -0500

    ftr-cliente-rest - Adding JPA and pagination to the Cliente RestController and its service.

--- src/main/java/com/cesarjuniort/springboot/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/cesarjuniort/springboot/apirest/models/dao/IClienteDao.java
@@ -1,7 +1,9 @@
 package com.cesarjuniort.springboot.apirest.models.dao;
 import com.cesarjuniort.springboot.apirest.models.entity.Cliente;
-import org.springframework.data.repository.CrudRepository;
 
-public interface IClienteDao extends CrudRepository<Cliente,Long>{
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
+
+public interface IClienteDao extends JpaRepository<Cliente,Long>{
 
 }
--- src/main/java/com/cesarjuniort/springboot/apirest/models/services/IClienteService.java
+++ src/main/java/com/cesarjuniort/springboot/apirest/models/services/IClienteService.java
@@ -2,12 +2,17 @@ package com.cesarjuniort.springboot.apirest.models.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 import com.cesarjuniort.springboot.apirest.models.entity.Cliente;
 
 public interface IClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
 	
 	public Cliente save(Cliente cliente);
 	
